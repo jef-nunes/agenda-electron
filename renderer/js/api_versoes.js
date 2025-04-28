e853c74054c3a3489bfd2539ec50122648461b24
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2025-04-28 16:04:41 -0300

    Create api_versoes.js

---
+++ renderer/js/api_versoes.js
@@ -0,0 +1,6 @@
+if(window.runtime.isDev){
+  console.log("[Versões]");
+  console.log(`Chrome: ${window.versions.chrome}`);
+  console.log(`NodeJS: ${window.versions.node}`);
+  console.log(`Electron: ${window.versions.electron}`);
+}
